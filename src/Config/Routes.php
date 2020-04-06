581ea06ee67a3f1ba8d17dd6335a8bd6b222a268
skuadron45 <zahidalharis@gmail.com>
2020-04-06 12:50:58 +0700

    Perbaikan routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -1,2 +1,14 @@
-<?php namespace Ci4adminrbac\Config;
+<?php
 
+namespace Ci4adminrbac\Config;
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->group('admin', [
+    "namespace" => "Ci4adminrbac\Controllers"
+], function (RouteCollection $routes) {
+    $routes->get('', 'Home');
+});
